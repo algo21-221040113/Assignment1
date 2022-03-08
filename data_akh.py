5b4067977bd84ae5357b048e081b52e65587427f
Wenter <221040113@link.cuhk.edu.cn>
2022-03-08 23:42:15 +0800

    Initial commit

---
+++ data_akh.py
@@ -0,0 +1,42 @@
+import akshare as ak
+import pandas as pd
+
+
+class AkData:
+    def __init__(self, sheet, start, end):
+        self.future_product = sheet
+        self.start_date = start
+        self.end_date = end
+
+    def get_data(self):
+        index = ['Day', 'Open', 'High', 'Low', 'Close', 'Vol', 'CCL', 'Dyn']
+        for i in self.future_product:
+            df = ak.futures_main_sina(symbol=i, start_date=self.start_date, end_date=self.end_date)
+            df.columns = index
+            df.to_csv(f"./Data/{i}.csv")
+
+
+class DataProcess:
+    def __init__(self, sheet):
+        self.lst = []
+        self.sheet = sheet
+        self.range = 100000
+
+        self.__get_data()
+
+    def __get_range(self):
+        for i in self.sheet:
+            df = pd.read_csv(f"./Data/{i}.csv")
+            self.range = min(self.range, len(df.index))
+
+    def __get_data(self):
+        self.__get_range()
+        for i in self.sheet:
+            df = pd.read_csv(f"./Data/{i}.csv")
+            del df['Unnamed: 0']
+            df = df.iloc[len(df.index) - self.range:]
+            df = df.reset_index(drop=True)
+            self.lst.append(df)
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from data_akh import AkData,DataProcess
+
+if __name__ == "__main__":
+    sheet = ["V0","P0","M0","I0","L0","C0","Y0","A0","J0","JM0","CS0","TA0","OI0","RM0","ZC0","SR0","CF0",
+             "MA0","FG0","SF0","SM0","FU0","AL0","RU0","ZN0","CU0","AU0","RB0","BU0","NI0","SN0"]
+
+    start_date = "20000101"
+    end_date = "20220101"
